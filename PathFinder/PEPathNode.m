a1877ef62d6769871d0862d4ab23f59e8f8af451
Chang-Gi, Kim <cgkim@mac.com>
2013-05-27 15:46:51 +0900

    path 중에 jump공간 채우기.

--- PathFinder/PEPathNode.m
+++ PathFinder/PEPathNode.m
@@ -12,6 +12,110 @@
 #import "PECommonUtil.h"
 
 
+@interface NSMutableArray (PathNodeAddition)
+
+
+- (void)addPositionsTo:(PEPathNode *)aNode;
+
+
+@end
+
+
+@implementation NSMutableArray (PathNodeAddition)
+
+
+- (void)addPositionsTo:(PEPathNode *)aNode
+{
+    PEPathNode *sParent = [aNode parent];
+
+    [self addObject:[aNode positionValue]];
+    
+    if (sParent)
+    {
+        CGPoint sPoint1 = [aNode position];
+        CGPoint sPoint2 = [sParent position];
+        CGPoint sVector = CGPointMake(sPoint1.x - sPoint2.x, sPoint1.y - sPoint2.y);
+        CGPoint sPoint  = sPoint1;
+        
+        sVector.x = (sVector.x == 0) ? 0 : (sVector.x > 0 ) ? -1 : 1;
+        sVector.y = (sVector.y == 0) ? 0 : (sVector.y > 0 ) ? -1 : 1;
+
+        for (NSInteger sIndex = 0; sIndex < MAX(sPoint1.x - sPoint2.x, sPoint1.y - sPoint2.y); sIndex++)
+        {
+            sPoint.x += sVector.x;
+            sPoint.y += sVector.y;
+            
+            if (CGPointEqualToPoint(sPoint, sPoint2))
+            {
+                break;
+            }
+            else
+            {
+                [self addObject:[NSValue valueWithCGPoint:sPoint]];
+            }
+        }
+    }
+    
+    
+#if (0)
+    if ([aNode parent])
+    {
+        PEPathNode *sParent = [aNode parent];
+        CGPoint     sPoint1 = [aNode position];
+        CGPoint     sPoint2 = [sParent position];
+        NSInteger   sXDelta = sPoint1.x - sPoint2.x;
+        NSInteger   sYDelta = sPoint1.y - sPoint2.y;
+        NSInteger   sXAbs   = abs((int)sXDelta);
+        NSInteger   sYAbs   = abs((int)sYDelta);
+        
+        if (sXAbs == 1 && sYAbs == 1)
+        {
+            [self addObject:[aNode positionValue]];
+        }
+        else
+        {
+            if (sXAbs > 1 && sYAbs > 1)
+            {
+                for (NSInteger x = 0; x < sXAbs; x++)
+                {
+                    NSInteger sX = (sXDelta > 0) ? -x : x;
+                    NSInteger sY = (sYDelta > 0) ? -x : x;
+                    
+                    CGPoint sPoint = CGPointMake(sPoint1.x + sX, sPoint1.y + sY);
+                    [self addObject:[NSValue valueWithCGPoint:sPoint]];
+                }
+            }
+            else if (sXAbs >= 1 && sYAbs == 0)
+            {
+                for (NSInteger x = 0; x < sXAbs; x++)
+                {
+                    NSInteger sX = (sXDelta > 0) ? -x : x;
+                    CGPoint sPoint = CGPointMake(sPoint1.x + sX, sPoint1.y);
+                    [self addObject:[NSValue valueWithCGPoint:sPoint]];
+                }
+            }
+            else if (sYAbs >= 1 && sXAbs == 0)
+            {
+                for (NSInteger y = 0; y < sYAbs; y++)
+                {
+                    NSInteger sY = (sYDelta > 0) ? -y : y;
+                    CGPoint sPoint = CGPointMake(sPoint1.x, sPoint1.y + sY);
+                    [self addObject:[NSValue valueWithCGPoint:sPoint]];
+                }
+            }
+        }
+    }
+    else
+    {
+        [self addObject:[aNode positionValue]];
+    }
+#endif
+}
+
+
+@end
+
+
 @implementation PEPathNode
 {
     CGPoint     mPosition;
@@ -100,20 +204,12 @@
 {
     NSMutableArray *sPath  = [NSMutableArray array];
     PEPathNode     *sNode  = self;
-    CGPoint         sPrevPoint;
-    CGPoint         sCurrPoint;
     
-    [sPath addObject:[sNode positionValue]];
-    sPrevPoint = [sNode position];
+    [sPath addPositionsTo:sNode];
     
     while ((sNode = [sNode parent]))
     {
-        sCurrPoint = [sNode position];
-        
-//        CGPoint sDelta = CGPointMake(sPrevPoint.x - sCurrPoint.x, sPrevPoint.y - sCurrPoint.y);
-//        NSLog(@"sDelta = %@", NSStringFromCGPoint(sDelta));
-        
-        [sPath insertObject:[sNode positionValue] atIndex:0];
+        [sPath addPositionsTo:sNode];
     }
     
     return sPath;
